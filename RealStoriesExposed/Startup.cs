2e9c5e956063ab86a70e021d831e1150d3dd40bb
Atanas Dimitrov <Atanas Dimitrov>
2016-11-28 12:21:28 +0200

    ASP.NET MVC Project created. Data and Model class libraries added.

---
+++ RealStoriesExposed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealStoriesExposed.Startup))]
+namespace RealStoriesExposed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
